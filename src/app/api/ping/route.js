c0e1a2ee2d99df09b24595ddc65fb5fa0f2b2065
Patrick <privers251@gmail.com>
2024-11-12 01:15:07 -0600

    Refreshed to apply ignore

--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function GET(request) {
     try {
         // Replace 'https://www.zohoapis.com/crm/v7/Leads' with your actual API endpoint
-        const response = await axios.get('https://www.zohoapis.com/crm/v7/Leads', {
+        const response = await axios.get('https://www.zohoapis.com/crm/v7/Leads?fields=Leads', {
             headers: {
                 Authorization: `Bearer ${process.env.ZOHO_ACCESS_TOKEN}`, // Replace with your access token variable
             }
