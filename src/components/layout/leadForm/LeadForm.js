d8a3b72833f783c31276c76ec2f2d5132972169b
Patrick <privers251@gmail.com>
2024-11-19 23:29:40 -0600

    Input validation. Submission message on form submission.

--- src/components/layout/leadForm/LeadForm.js
+++ src/components/layout/leadForm/LeadForm.js
@@ -7,7 +7,6 @@ import './LeadForm.css'
 export default function QuoteForm() {
     // State for form data and error messages
     const [formData, setFormData] = useState({
-        First_Name: '',
         Last_Name: '',
         Email: '',
         Phone: '',
@@ -16,6 +15,9 @@ export default function QuoteForm() {
         State: '',
         Zip_Code: '',
     });
+
+    const [isSubmitted, setIsSubmitted] = useState(0);
+    const submittedMessage = 'Thank you so much for reaching out a member of our team will be with you shortly.'
    
     const [errorMessage, setErrorMessage] = useState('')
 
@@ -37,9 +39,9 @@ export default function QuoteForm() {
     // Handle form submission
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        
+               
         try {
+            setIsSubmitted(isSubmitted + 1)
             const response = await axios.post('/api/leads', formData);
             console.log(response)
         } catch (error) {
@@ -50,34 +52,34 @@ export default function QuoteForm() {
     
 
     return (
-        <section className="LeadFormContainer">
+        <section className="LeadFormSection">
             <div>
                 <h1>Schedule Your FREE Inspection!</h1>
             </div>
-          
+
             <div className="flex justify-center">
+
+                {isSubmitted == 1 ? <p className='submittedMessage'>{submittedMessage}</p>:
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="First_Name"></label>
-                    <input type="text" id="First_Name" name="First_Name" placeholder="First Name" onChange={handleChange} />
 
                     <label htmlFor="Last_Name"></label>
-                    <input type="text" id="Last_Name" name="Last_Name" placeholder="Last Name" onChange={handleChange} />
+                    <input type="text" id="Last_Name" name="Last_Name" placeholder="Name" onChange={handleChange} />
 
                     <label htmlFor="Email"></label>
                     <input type="email" id="Email" name="Email" placeholder="Email" onChange={handleChange} />
 
                     <label htmlFor="Phone"></label>
-                    <input type="tel" id="Phone" name="Phone" placeholder="Phone Number" onChange={handleChange} />
+                    <input type="tel" id="Phone" name="Phone" placeholder="Phone Number" maxLength={10} onChange={handleChange} />
 
                     <label htmlFor="Street"></label>
-                    <input type="text" id="Street" name="Street" placeholder="Street Address" onChange={handleChange} />
+                    <input type="text" id="Street" name="Street" placeholder="Street Address" maxLength={50} onChange={handleChange} />
 
                     <label htmlFor="City"></label>
-                    <input type="text" id="City" name="City" placeholder="City" onChange={handleChange} />
+                    <input type="text" id="City" name="City" placeholder="City" maxLength={15} onChange={handleChange} />
 
                     <label htmlFor="State"></label>
                     <input type="text" id="State" name="State" placeholder="State" maxLength={2} onChange={handleChange} />
-                    {errorMessage && <p className="QuoteFormContainerError">{errorMessage}</p>}
+                    {errorMessage && <p className="LeadFormContainerError">{errorMessage}</p>}
 
                     <label htmlFor="Zip_Code"></label>
                     <input type="text" id="Zip_Code" name="Zip_Code" placeholder="Zip Code" maxLength="5" pattern="\d{5}" onChange={handleChange} />
@@ -86,6 +88,7 @@ export default function QuoteForm() {
                         <button type="submit" id="submit" className="submit" aria-label="Submit" title="Submit">Submit</button>
                     </div>
                 </form>
+}
             </div>
         </section>
     );
