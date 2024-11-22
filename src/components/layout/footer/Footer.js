9680f7e644de40210bb298e572afccee64b9d8d5
Patrick <privers251@gmail.com>
2024-11-21 23:48:57 -0600

    Fixed footer links

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -8,19 +8,18 @@ export default function Footer(){
             <div className="flex justify-around mt-5 items-center pb-5">
             <ul className="flex space-x-7">
                 <li>
-                    <a href="#"><i class="fa-brands fa-facebook text-3xl text-slate-200 footerIcon"></i></a> 
+                    <a href="https://www.facebook.com/profile.php?id=61564218812242"><i class="fa-brands fa-facebook text-3xl text-slate-200 footerIcon"></i></a> 
                 </li>
                 <li>
-                    <a href="#"><i class="fa-brands fa-instagram text-3xl text-slate-200 footerIcon"></i></a> 
+                    <a href="mailto:Stormshieldbuildersllc@gmail.com"><i class="fa-regular fa-envelope text-3xl text-slate-200 footerIcon"></i></a> 
                 </li>
             </ul>
             </div>
 
             <div className="signature">
-                <p>Powered by: <a
-                href="https://www.github.com/privers251"
-                target="_blank"
-                >Patrick Rivers</a></p>
+                <p>Powered by: 
+                <a href="https://www.github.com/privers251"
+                target="_blank">Patrick Rivers</a></p>
             </div>
         </footer>
     )
