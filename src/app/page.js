0109ff1c8c91c3fe6c3d2e4b0cc44a4e16c16fb9
Patrick <privers251@gmail.com>
2024-11-28 10:58:42 -0600

    Temporaritly removed google reviews section.

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
         <Hero />
         <LeadForm />
         <Services />
-        <Reviews />
+        {/* <Reviews /> */}
         <AboutUs />
     </div>
   );
