bd50c4da4bfef7915b112524b7d056b617c7be05
Patrick <privers251@gmail.com>
2024-12-13 17:43:47 -0600

    Updated the Google Reviews Widget to a free option.

--- src/app/home/components/Reviews.js
+++ src/app/home/components/Reviews.js
@@ -6,8 +6,10 @@ export default function Reviews(){
     
     return (
         <div className='reviewsSectionWrapper'>
-            <div><h1>Reviews</h1></div>
-            <div className='reviewsSection' src='https://cdn.trustindex.io/loader.js?90e4988371b34980d2665a0f647'></div>
+            <div className='reviewsSectionHeader'><h1>Reviews</h1></div>
+            {/* <div className='reviewsSection' src='https://cdn.trustindex.io/loader.js?90e4988371b34980d2665a0f647'></div> */}
+            <script src="https://cdn.commoninja.com/sdk/latest/commonninja.js" defer></script>
+<div class="commonninja_component pid-59f7a69d-98f9-4dff-934a-46060ac24660"></div>
         </div>
     )
 }
